cbfb3918003cf51765d1513121811acbaebc9327
Alex <ictosaka2813@abroadschools.jp>
2025-09-26 10:41:08 +0900

    Button yipeee

---
+++ v_box_container.gd
@@ -0,0 +1,20 @@
+extends VBoxContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass # Replace the function body
+
+
+
+func _on_button_pressed() -> void:
+	pass # Replace  with function body
+
+
+func _on_button_2_button_up() -> void:
+	pass # Replace with function body.
