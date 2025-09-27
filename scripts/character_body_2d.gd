68318f97fcfaefb7aaaecb1714d1231615d10b44
Sasha <juistals52@gmail.com>
2025-09-27 13:10:52 +0900

    movement

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
 var speed : float = 100.0
+var direction : Vector2
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _process(delta: float):
-	var direction : Vector2 = Vector2.ZERO
+func _physical_process(_delta):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = direction * speed
-	if direction.x and direction.y == 0:
+	
+	if direction.x > 0: animated_sprite_2d.flip_h = false
+	if direction.x < 0: animated_sprite_2d.flip_h = true
+	
+	if direction.x == 0 and direction.y == 0:
 		animated_sprite_2d.play("idle")
-
-func _physics_process(delta: float):
+	else:
+		animated_sprite_2d.play("move")
 	move_and_slide()
