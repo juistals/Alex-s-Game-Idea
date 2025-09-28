dd9adbc868769d0f2837d5783a01130f584fcead
Sasha <juistals52@gmail.com>
2025-09-28 19:34:18 +0900

    upd

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,16 +4,25 @@ var speed : float = 100.0
 var direction : Vector2
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-func _physical_process(_delta):
+@warning_ignore("unused_parameter")
+func _physics_process(delta):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	# Normalize to avoid faster diagonal movement
+	if direction != Vector2.ZERO:
+		direction = direction.normalized()
+
 	velocity = direction * speed
+
+	if direction.x > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction.x < 0:
+		animated_sprite_2d.flip_h = true
 	
-	if direction.x > 0: animated_sprite_2d.flip_h = false
-	if direction.x < 0: animated_sprite_2d.flip_h = true
-	
-	if direction.x == 0 and direction.y == 0:
+	if direction == Vector2.ZERO:
 		animated_sprite_2d.play("idle")
 	else:
 		animated_sprite_2d.play("move")
+
 	move_and_slide()
