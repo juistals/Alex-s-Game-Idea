96b728c6cacb1b4c74f328459b98c1b5679545d0
Jason <8545019@gmail.com>
2025-11-04 03:16:58 +0900

    sth
    blah blah blah

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,22 @@
 class_name Players extends CharacterBody2D
 
 var speed : float = 100.0
+var sprint_speed : float = 160.0  
 var direction : Vector2
 @onready var animated_sprite_2d: Sprite2D = $Sprite2D
 
-
-@warning_ignore("unused_parameter")
 func _physics_process(delta):
 	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	
-	# Normalize to avoid faster diagonal movement
 	if direction != Vector2.ZERO:
 		direction = direction.normalized()
 
-	velocity = direction * speed
+	var current_speed = speed
+	if Input.is_action_pressed("sprint"): 
+		current_speed = sprint_speed
+
+	velocity = direction * current_speed
 
 	if direction.x > 0:
 		animated_sprite_2d.flip_h = true
