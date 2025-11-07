3ac2503c36a6d1399e206f1847d8791685c8d434
Alex <ictosaka2813@abroadschools.jp>
2025-11-07 12:04:32 +0900

    4545

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -17,3 +17,10 @@ func _on_button_2_pressed():
  
 func _on_button_3_pressed():
 	get_tree(). quit()
+
+
+
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://4545.tscn")
