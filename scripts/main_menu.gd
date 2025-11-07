a131f1e0802e1c723036a7d00e06cfe6351fdcf6
Alex <ictosaka2813@abroadschools.jp>
2025-11-07 11:18:20 +0900

    Audio

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,7 +9,7 @@ func _on_button_pressed():
 
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://options.tscn") 
+	get_tree().change_scene_to_file("res://Scenes/options.tscn") 
 
 
  
